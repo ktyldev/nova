7e8129705a70a5d9bbbd42a1c0fefbdc2b991e69
Cat Flynn <cathalflynn@hotmail.co.uk>
2019-02-02 15:26:54 +0000

    Basic player movement

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     // TODO:
-    // aim dir
     // move dir
+    // aim weapons
+    // (weapons will probably follow the mouse more closely)
     // is shooting
 
+    // TODO: 
+    // make movement more sophisticated. it should be faster for the ship
+    // to accelerate forwards, not equal in every direction
+    public float acceleration;
+    [Range(0, 1)]
     public float rotationSpeed;
 
     private Rigidbody2D _rb;
     // the direction the player is trying to move
     private Vector2 _moveInput;
-    // the direction the player is trying to aim
-    // weapons will probably follow this more closely
-    private Vector2 _aimDirection;
 
     private void Awake()
     {
@@ -25,20 +28,25 @@ public class PlayerInput : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // move
-        // aim
+        Movement();
         Rotate();
+        // aim weapons
     }
 
-    private void MoveInput()
+    private void Movement()
     {
+        var input = new Vector2(
+            Input.GetAxis(GameConstants.Horizontal), 
+            Input.GetAxis(GameConstants.Vertical));
+
+        _rb.AddForce(input * acceleration);
     }
 
     private void Rotate()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // unity's 2D is dumb
         mousePos.z = 0;
-        print(mousePos);
 
         var aimDir = (mousePos - transform.position).normalized;
         var lookDir = Vector3.Lerp(transform.up, aimDir, rotationSpeed);
