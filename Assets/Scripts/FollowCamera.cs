d6703e4ad7ec8a05a7dc088624ccff96f64f728f
Cat Flynn <cathalflynn@hotmail.co.uk>
2019-02-02 19:46:53 +0000

    add basic camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    [Range(0, 1)]
+    public float smoothing;
+
+    private float _z;
+
+    private void Awake()
+    {
+        _z = transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        var targetPosition = target.transform.position;
+        targetPosition.z = _z;
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPosition,
+            smoothing);
+    }
+}
