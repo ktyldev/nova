3c3ac63f63536c9aa58a78f6e51f9e1b908fb5dc
Cat Flynn <cathalflynn@hotmail.co.uk>
2019-02-02 20:47:22 +0000

    fire ma lazor

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    // TODO: maybe we need a proper Laser class
+    [Serializable]
+    public struct Laser
+    {
+        public GameObject laser;
+        public float length;
+        [HideInInspector]
+        public LineRenderer line => laser.GetComponent<LineRenderer>(); 
+    }
+
+    public Transform muzzle;
+
+    [Header("Lasers")]
+    public Laser targetingLaser;
+    public Laser weaponLaser;
+
     private PlayerInput _input;
+    private bool _isFiring;
 
     private void Start()
     {
@@ -15,16 +33,45 @@ public class Weapon : MonoBehaviour
     {
         if (_input.GetStartFiring())
         {
-            StartCoroutine(Fire());
+            StartCoroutine(FireLaser());
         }
     }
 
-    private IEnumerator Fire()
+    private void LateUpdate()
     {
-        while (_input.GetFiring())
+        DrawLasers();
+    }
+
+    private void DrawLasers()
+    {
+        weaponLaser.line.positionCount = 0;
+        targetingLaser.line.positionCount = 0;
+
+        if (_isFiring)
         {
-            print("pew!");
-            yield return new WaitForEndOfFrame();
+            DrawLaser(weaponLaser);
         }
+        else
+        {
+            DrawLaser(targetingLaser);
+        }
+    }
+
+    private void DrawLaser(Laser laser)
+    {
+        laser.line.positionCount = 2;
+
+        var start = muzzle.transform.position;
+        var end = start + muzzle.up * laser.length;
+        var positions = new[] { start, end };
+
+        laser.line.SetPositions(positions);
+    }
+
+    private IEnumerator FireLaser()
+    {
+        _isFiring = true;
+        yield return new WaitWhile(() => _input.GetFiring());
+        _isFiring = false;
     }
 }
