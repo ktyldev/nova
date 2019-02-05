687c04e04d22c65716c787796c61f6507a5669a3
Cat Flynn <cathalflynn@hotmail.co.uk>
2019-02-05 13:04:30 +0000

    create empty input for enemy

---
+++ Assets/Scripts/EnemyInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInput : MonoBehaviour, IInputProvider
+{
+    public Vector2 AccelerationDir => Vector2.zero;
+
+    public Vector2 TargetPosition => Vector2.zero;
+
+    public bool IsFiring => false;
+}
