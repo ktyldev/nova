714e6c1724134b5571d9589cf38b6340e3106ea8
Cat Flynn <me@monodokimes.uk>
2019-03-12 13:17:08 +0000

    start splitting up weapon code

---
+++ Assets/Scripts/LaserWeapon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LaserWeapon : Weapon
+{
+    [Tooltip("how often damage is dealt, per second")]
+    public float damageFrequency = 5f;
+
+    public GameObject laser;
+
+    private float _damageInterval;
+    private Laser _laser;
+
+    private void Awake()
+    {
+        _damageInterval = 1f / damageFrequency;
+    }
+
+    private void Start()
+    {
+        _laser = laser.GetComponent<Laser>();
+        _laser.SetActive(false);
+    }
+
+    protected override IEnumerator DoFire(Func<bool> getFiring)
+    {
+        _laser.SetActive(true);
+
+        float elapsed = 0;
+        float chunk = damage * _damageInterval;
+
+        while (getFiring())
+        {
+            yield return new WaitForEndOfFrame();
+
+            elapsed += Time.deltaTime;
+            if (elapsed < _damageInterval)
+                continue;
+
+            // go back so the interval check will fail next time
+            elapsed -= _damageInterval;
+
+            // damage interval has passed, look for a health component and smack it
+            var health = _laser.Occluder?.GetComponent<Health>();
+            if (health == null)
+                continue;
+
+            health.TakeDamage(chunk);
+        }
+
+        _laser.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Transform muzzle;
+    public float damagePerSecond;
+    public float bulletDamage;
+
+    public GameObject weaponGun;
+    [Header("Lasers")]
+    public GameObject targeting;
+
+    private IInputProvider _input;
+    private bool _isFiring;
+    private bool _laserActive = true;
+    private bool _gunActive;
+
+    private Gun _gun;
+    private Laser _targeting;
+    private Ship _ship;
+
+    private Weapon _laserWeapon;
+
+    private void Start()
+    {
+        _ship = GetComponentInParent<Ship>();
+        _input = _ship.InputProvider;
+
+        _targeting = targeting.GetComponent<Laser>();
+
+        _gun = weaponGun.GetComponent<Gun>();
+
+        _laserWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    void Update()
+    {
+        //if (!_ship.isLocalPlayer)
+        //    return;
+
+        if (_input.IsFiring)
+        {
+            if(_laserActive == true)
+            {
+                _laserWeapon.Fire(_input);
+            }
+
+            if (_gunActive == true)
+            {
+                StartCoroutine(FireBullet());
+            }
+        }
+    }
+
+    private IEnumerator FireBullet()
+    {
+        _targeting.SetActive(false);
+        float elapsed = 0;
+        float fireInterval = 0.3f;
+
+        while(_input.IsFiring){
+            elapsed += Time.deltaTime;
+            if (elapsed < fireInterval)
+            {
+                _gun.Fire();
+            }
+
+            elapsed -= fireInterval;
+
+            _isFiring = false;
+            _targeting.SetActive(true);
+            yield break;
+        }
+    }
+}
