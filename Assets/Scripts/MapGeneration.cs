c04583cb72c6b7dd4dd9b6ead03a5d9b557881a5
Cat Flynn <me@monodokimes.uk>
2019-04-11 00:44:48 +0100

    don't spawn asteroids near player start

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Networking;
 
 public class MapGeneration : MonoBehaviour
 {
@@ -19,33 +17,50 @@ public class MapGeneration : MonoBehaviour
     public Bounds mapBounds;
     public float minScale, maxScale;
     public float minSeperation;
+    public float centreRadius;
 
-    private List<GameObject> _spawned;
+    private List<GameObject> _spawned = new List<GameObject>();
 
     private void Start()
     {
         GenerateAsteroids();
     }
 
+    private Vector3 SpawnPosition
+    {
+        get
+        {
+            Vector3 pos = Vector3.zero;
+            do
+            {
+                float x = Random.Range(mapBounds.min.x, mapBounds.max.x);
+                float y = Random.Range(mapBounds.min.y, mapBounds.max.y);
+
+                // bad position, skip it
+                pos = new Vector3(x, y, 0);
+                if (pos.magnitude < centreRadius)
+                    continue;
+
+                if (_spawned.Any(a => Vector3.Distance(a.transform.position, pos) < minSeperation))
+                    continue;
+
+            } while (pos == Vector3.zero);
+
+            return pos;
+        }
+    }
+
     private void GenerateAsteroids()
     {
-        _spawned = new List<GameObject>();
-
         for (int i = 0; i < maxToGenerate; i++)
         {
-            float x  = Random.Range(mapBounds.min.x, mapBounds.max.x);
-            float y  = Random.Range(mapBounds.min.y, mapBounds.max.y);
-            var pos  = new Vector3(x, y, 0);
+
             float size = Random.Range(minScale, maxScale);
             int s = Random.Range(0, asteroids.sprites.Length);
 
-            if (_spawned.Any() &&
-                _spawned.Any(a => Vector3.Distance(a.transform.position, pos) < minSeperation))
-                continue;
-
             var ast = Instantiate(asteroids.template);
             ast.transform.SetParent(asteroids.parent);
-            ast.transform.localPosition = pos;
+            ast.transform.localPosition = SpawnPosition;
             ast.transform.localScale = new Vector3(size, size);
             ast.transform.Rotate(0, 0, Random.Range(0, 360));
 
