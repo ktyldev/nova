d2ab238b835b1a8a45517d6ca4625cd980178857
Cat Flynn <me@monodokimes.uk>
2019-04-22 17:56:17 +0100

    indicate hull damage with dimming/flickering

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,12 +6,16 @@ using UnityEngine.Networking;
 public class Ship : MonoBehaviour
 {
     public GameObject[] weaponIndicators;
-    private LightPanel[] _weaponIndicatorPanels;
     public GameObject[] heatIndicators;
-    private LightPanel[] _heatIndicatorPanels;
-
+    public GameObject[] hullIndicators;
+    public float hullFlickerSpeed = 10f;
+    public float hullFlickerThreshold = 0.2f;
     [Range(0, 1)]
     public float weaponRotationSpeed = 0.55f;
+
+    private LightPanel[] _weaponIndicatorPanels;
+    private LightPanel[] _heatIndicatorPanels;
+    private LightPanel[] _hullIndicatorPanels;
 
     public Heat Heat { get; private set; }
     public IInputProvider InputProvider { get; private set; }
@@ -22,16 +26,22 @@ public class Ship : MonoBehaviour
 
     // cheap hack
     private WeaponController _guideWeapon;
+    private Health _health;
 
     private void Awake()
     {
+
         InputProvider = GetComponent<IInputProvider>();
         Movement = GetComponent<ShipMovement>();
+        _health = GetComponent<Health>();
 
         _weaponIndicatorPanels = weaponIndicators
             .Select(go => go.GetComponent<LightPanel>())
             .ToArray();
         _heatIndicatorPanels = heatIndicators
+            .Select(go => go.GetComponent<LightPanel>())
+            .ToArray();
+        _hullIndicatorPanels = hullIndicators
             .Select(go => go.GetComponent<LightPanel>())
             .ToArray();
 
@@ -58,12 +68,29 @@ public class Ship : MonoBehaviour
         {
             p.Colour = Heat.IndicatorColour;
         }
+
+        var hullColour = GetHullColour();
+        foreach (var p in _hullIndicatorPanels)
+        {
+            p.Colour = hullColour;
+        }
+    }
+
+    private Color _white = new Color(1, 1, 1, 1);
+    private Color GetHullColour()
+    {
+        float flickerSpeed = 10f;
+        float flickerThreshold = 0.2f;
+
+        if (_health.Normalised > flickerSpeed)
+            return _health.Normalised * _white;
+
+        return Mathf.Abs(Mathf.PerlinNoise(Time.time * flickerSpeed, 0)) * _white;
     }
 
     // TODO: play explosion sound
     public void Die()
     {
-        //Explosion.New(Game.Instance.shipExplosion, transform.position);
         Game.Instance.GameOver();
     }
 }
